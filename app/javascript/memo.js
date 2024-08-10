6958e6ee9da40cf00fd59f18cacbd8d964189a9a
yuuuuuuuka <yuuuuka.02.29@gmail.com>
2024-08-10 21:35:23 +0900

    memo.jsの復元

--- app/javascript/memo.js
+++ app/javascript/memo.js
@@ -1,10 +1,6 @@
 const buildHTML = (XHR) => {
   const item = XHR.response.post;
   const html = `
-<<<<<<< Updated upstream
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
     <div class="post">
       <div class="post-date">
         投稿日時：${item.created_at}
@@ -16,30 +12,6 @@ const buildHTML = (XHR) => {
   return html;
 };
 
-=======
-=======
->>>>>>> 6397c045a0407241223db5b5657ac5ece2a10106
->>>>>>> Stashed changes
-  <div class="post">
-    <div class="post-date">
-      投稿日時：${item.created_at}
-    </div>
-    <div class="post-content">
-      ${item.content}
-    </div>
-  </div>`;
-  return html;
-};
-
-
-
-<<<<<<< Updated upstream
-=======
-<<<<<<< HEAD
->>>>>>> 6397c045a0407241223db5b5657ac5ece2a10106
-=======
->>>>>>> 6397c045a0407241223db5b5657ac5ece2a10106
->>>>>>> Stashed changes
 function post() {
   const form = document.getElementById("form");
   form.addEventListener("submit", (e) => {
@@ -50,31 +22,12 @@ function post() {
     XHR.responseType = "json";
     XHR.send(formData);
     XHR.onload = () => {
-<<<<<<< Updated upstream
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-      const list = document.getElementById("list");
-      const formText = document.getElementById("content");
-=======
-=======
->>>>>>> 6397c045a0407241223db5b5657ac5ece2a10106
->>>>>>> Stashed changes
       if (XHR.status != 200) {
         alert(`Error ${XHR.status}: ${XHR.statusText}`);
         return null;
       };
       const list = document.getElementById("list");
       const formText = document.getElementById("content");
-
-      list.insertAdjacentHTML("afterend", html);
-<<<<<<< Updated upstream
-=======
-<<<<<<< HEAD
->>>>>>> 6397c045a0407241223db5b5657ac5ece2a10106
-=======
->>>>>>> 6397c045a0407241223db5b5657ac5ece2a10106
->>>>>>> Stashed changes
       list.insertAdjacentHTML("afterend", buildHTML(XHR));
       formText.value = "";
     };
